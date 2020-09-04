5cce3d919f0bf2cb66aaffdb2c3da9140a300abe
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-09-03 22:25:53 -0500

    Preliminary Plot Creation

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// set the dimensions and margins of the graph
+var svg
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 1500 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#line")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+var promises = [];
+promises.push(d3.csv("data/Triple_Doubles_Totals.csv"))
+Promise.all(promises).then(callback);
+
+function callback(data){
+  total = data[0]
+  console.log(total)
+
+  var x = d3.scaleTime()
+  .domain([1979,2020])
+  .range([ 0, width ]);
+  svg.append("g")
+  .attr("stroke", "white")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+  
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 200])
+    .range([ height, 0 ]);
+  svg.append("g")
+  .attr("stroke", "white")
+    .call(d3.axisLeft(y));
+
+  // Add the line
+  svg.append("path")
+    .datum(total)
+    .sort(function(a, b){
+      //this function sorts from highest to lowest values
+      return b.Total.Year - a.Total.Year
+      })
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function(d) { return x(d.Year)})
+      .y(function(d) { return y(d.Count) })
+      .curve(d3.curveMonotoneX)
+      )
+
+}
+
